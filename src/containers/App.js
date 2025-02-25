cdc6ae92e5133ace0e583169ced3edee0aa37b2a
Alejunder <chechis.cole@gmail.com>
2025-02-25 22:03:27 +0100

    add github pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,8 @@ class App extends Component {
 
   componentDidMount() {
     // fetch('https://jsonplaceholder.typicode.com/users')
-    fetch('data/robots.json')
-      .then(response=> response.json())
+    fetch('/data/robots.json')
+      .then(response => response.json())
       .then(users => {this.setState({ robots: users})});
   }
 
